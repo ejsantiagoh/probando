81327ae434818828c4b83a8d873dfe73b3cb41cb
ejsantiagoh <eimerjesussh@hotmail.com>
2025-06-13 21:20:07 -0500

    subiendo proyecto estado inicial

---
+++ backend/src/test/java/com/c4/atunesdelpacifico/AtunesdelpacificoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4.atunesdelpacifico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtunesdelpacificoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
