02e359f2b0275c6d17104d66c8b2635cd24d67fe
ejsantiagoh <eimerjesussh@hotmail.com>
2025-06-13 21:26:43 -0500

    cambios en el main

--- backend/src/main/java/com/c4/atunesdelpacifico/AtunesdelpacificoApplication.java
+++ backend/src/main/java/com/c4/atunesdelpacifico/AtunesdelpacificoApplication.java
@@ -9,10 +9,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class AtunesdelpacificoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		/*Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));*/
 		SpringApplication.run(AtunesdelpacificoApplication.class, args);
 	}
 
